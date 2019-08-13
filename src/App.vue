62860d4dabb4b719b11f73d30626ce7707c56cb2
Valchan <valeria_padilha@yahoo.com.br>
2019-08-13 15:53:09 -0300

    Adequando o template gerado

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="col-md-6 px-0 text-center">
+      <h1>{{ title }}</h1>
+      <ul class="list-group">
+        <li class="list-group-item" v-for="img of imgs">
+          <img :src="img.url" :alt="img.title"  class="img-fluid rounded">
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data(){
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'Calopsita',
+      imgs: [
+        {
+          url: './src/assets/dog.png',
+          title: 'Dog'
+        },
+        {
+          url: './src/assets/cat.jpg',
+          title: 'Cat'
+        }
+      ]
     }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
