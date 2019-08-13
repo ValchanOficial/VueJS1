29dbdbdc35291577a3975d5be8f6ff694ff3225f
Valchan <valeria_padilha@yahoo.com.br>
2019-08-13 16:31:20 -0300

    Integração com API de terceiros

--- README.md
+++ README.md
@@ -8,6 +8,9 @@
 # install dependencies
 npm install
 
+# API serve at localhost:3000
+npm start
+
 # serve with hot reload at localhost:8080
 npm run dev
 
---
+++ vuejs/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="col-md-4 px-0 text-center">
+        <img v-for="img of imgs" :src="img.url" :alt="img.title"  class="img-thumbnail border img-fluid rounded">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      imgs: []
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(imgs => this.imgs = imgs, err => console.log(err)); //res.json // imgs // imgs.json
+  }
+}
+</script>
+
+<style>
+
+</style>
